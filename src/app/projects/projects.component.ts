4e4c5c22664b0a2f94a309742092296158be20d1
NABIJON <79036335+Nabijon2001@users.noreply.github.com>
2022-03-30 10:28:18 +0500

    Document Format

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,94 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   experiments = [
     {
       name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
+      technologies: ['Html', 'Css', 'Bootstrap', 'Typescript', 'Angular'],
       link: '',
       img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
+      title: 'Ecommerce Web design with admin',
     },
     {
       name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
+      technologies: ['Html', 'Css', 'Bootstrap', 'Typescript', 'Angular'],
       link: '',
       img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
-    },  {
-      name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
-      link: '',
-      img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
-    },  {
-      name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
-      link: '',
-      img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
-    },{
-      name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
-      link: '',
-      img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
-    },{
-      name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
-      link: '',
-      img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
-    },{
-      name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
-      link: '',
-      img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
-    },{
-      name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
-      link: '',
-      img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
-    },{
-      name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
-      link: '',
-      img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
-    },{
-      name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
-      link: '',
-      img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
-    },{
-      name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
-      link: '',
-      img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
-    },{
-      name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
-      link: '',
-      img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
-    },{
-      name: 'Dinopark',
-      technologies: ["Html", "Css", "Bootstrap", "Typescript","Angular"],
-      link: '',
-      img: 'https://picsum.photos/300/350',
-      title: 'Ecommerce Web design with admin'
+      title: 'Ecommerce Web design with admin',
     },
-  ]
+    {
+      name: 'Dinopark',
+      technologies: ['Html', 'Css', 'Bootstrap', 'Typescript', 'Angular'],
+      link: '',
+      img: 'https://picsum.photos/300/350',
+      title: 'Ecommerce Web design with admin',
+    },
+    {
+      name: 'Dinopark',
+      technologies: ['Html', 'Css', 'Bootstrap', 'Typescript', 'Angular'],
+      link: '',
+      img: 'https://picsum.photos/300/350',
+      title: 'Ecommerce Web design with admin',
+    },
+  ];
 }
